05369346c925e00f2b16cd9b2194df56f9254881
MarwaElbeshry <marwaelbeshry@gmail.com>
2017-12-14 09:39:49 +0200

    Add files via upload

---
+++ cardData.js
@@ -0,0 +1,580 @@
+[
+   
+    {
+      "section": "1",
+      "title": "A ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "Critical",
+      "assignedTo": "  مروة البشرى البشرى البشرى",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"true",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+     
+    },
+    {
+      "section": "2",
+      "title": "A ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "Critical",
+      "assignedTo": "  مروة البشرى البشرى البشرى",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"false",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+      
+    },
+    {
+      "section": "1",
+      "title": "A ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "normal",
+      "assignedTo": "  مروة البشرى البشرى البشرى",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"true",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "1",
+      "title": "A ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "Critical",
+      "assignedTo": "  مروة البشرى البشرى البشرى",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"true",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "1",
+      "title": "A ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "normal",
+      "assignedTo": "  مروة البشرى البشرى البشرى",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"false",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "0",
+      "title": "A ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "critical",
+      "assignedTo": "  مروة البشرى البشرى البشرى",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"true",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "1",
+      "title": "B ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "Critical",
+      "assignedTo": "باكر",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"false",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "2",
+      "title": "C ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "Critical",
+      "assignedTo": "احمد امين امين امين",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"false",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "3",
+      "title": "D ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "Critical",
+      "assignedTo": "احمد الباز الباز",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"true",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "3",
+      "title": "E ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "Critical",
+      "assignedTo": "محمد فريد ",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"false",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "1",
+      "title": "E ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "Normal",
+      "assignedTo": "ابراهيم  ابراهيم",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"true",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "0",
+      "title": "E ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "Normal",
+      "assignedTo": "ابراهيم  ابراهيم",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"false",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "0",
+      "title": "E ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "Normal",
+      "assignedTo": "ابراهيم  ابراهيم",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"false",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "2",
+      "title": "E ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "Normal",
+      "assignedTo": "ابراهيم  ابراهيم",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"false",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "2",
+      "title": "E ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "Normal",
+      "assignedTo": "ابراهيم  ابراهيم",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"false",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "2",
+      "title": "E ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "Normal",
+      "assignedTo": "ابراهيم  ابراهيم",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"false",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "2",
+      "title": "E ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "low",
+      "assignedTo": "ابراهيم  ابراهيم",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"false",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "2",
+      "title": "E ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "low",
+      "assignedTo": "اشرف  اشرف اشرف",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"false",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "0",
+      "title": "E ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "Critical",
+      "assignedTo": "ابراهيم  ابراهيم",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"false",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "0",
+      "title": "E ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "Critical",
+      "assignedTo": "ابراهيم  ابراهيم",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"false",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "0",
+      "title": "E ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "Critical",
+      "assignedTo": "دينا   دينا دينا ",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"false",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "0",
+      "title": "E ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "Critical",
+      "assignedTo": "مروة  مروة مروة",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"false",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "1",
+      "title": "E ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "low",
+      "assignedTo": "باكر  باكر باكر باكر",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"false",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "1",
+      "title": "E ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "low",
+      "assignedTo": "باكر  باكر باكر باكر",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"false",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    },
+    {
+      "section": "1",
+      "title": "E ترقيه السيرفر للنسخه الجديدةليتوافق ع أحتياحات نسخ’ البرنامج الجديد",
+      "Status": "low",
+      "assignedTo": "باكر  باكر باكر باكر",
+      "features": [
+        {  
+          "feature":"Discussing",
+          "color":"#5498fd"
+       },
+       {  
+          "feature":"rejected",
+          "color":"#ed8175"
+       }
+      ],
+      "Important":"false",
+      "dates" :{
+        "StartDate":"12/5/2014",
+        "EndtDate":"13/5/2014",
+        "SuggestedtDate":"12/5/2014",
+        "ActualDate":"15/5/2014"
+      }
+    }
+  ]
